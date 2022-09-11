3c100b66f64ce8aa83e6d51362516c1bee40e2fa
Ben DeSollar <ben-desollar@uiowa.edu>
2022-09-11 17:10:35 -0500

    Updated chart, but more functionality needed to update data points

--- thermometer-ui/src/App.js
+++ thermometer-ui/src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import logo from './logo.svg'
+import React from 'react';
 import './App.css';
 import Navbar from "./components/pages/Navbar";
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
