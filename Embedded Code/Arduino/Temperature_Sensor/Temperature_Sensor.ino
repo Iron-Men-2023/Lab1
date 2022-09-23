f3e60e5e2703089be5606ea01480efa08768ae91
Adnane Ezouhri <adnane-ezouhri@uiowa.edu>
2022-09-23 16:02:04 -0500

    Update Temperature_Sensor.ino
    Finished hardware and related code with @Justin111300

--- Embedded Code/Arduino/Temperature_Sensor/Temperature_Sensor.ino
+++ Embedded Code/Arduino/Temperature_Sensor/Temperature_Sensor.ino
@@ -9,8 +9,8 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-String celsius_temp="";
-String farenheit_temp="";
+String celsius_temp = "";
+String farenheit_temp = "";
 
 int switch_value = 0;
 const int switch_pin = 10;
@@ -21,8 +21,9 @@ const int button_pin = 11;
 int sensor_value = 0;
 const int senor_pin = 3;
 
-void setup(void)
-{
+String switch_fb = "true";
+
+void setup(void) {
   lcd.display();
   //for switch
   pinMode(switch_pin, INPUT);
@@ -33,8 +34,7 @@ void setup(void)
   Serial.begin(9600);
 }
 
-void loop(void)
-{
+void loop(void) {
 
   switch_value = digitalRead(10);
   button_value = digitalRead(11);
@@ -45,41 +45,56 @@ void loop(void)
   double temp_celsius = sensors.getTempCByIndex(0);
   double temp_farenheit = (temp_celsius * 9.0) / 5.0 + 32.0;
 
-  if (switch_value == HIGH)
-  {
-    // Send a value to pi to start up data in firebase
-    // And start listening to arduino for temp
 
-    if (button_value == HIGH)
-    {
-      if (temp_celsius == -127)
-      {
-        lcd.setCursor(0, 0);
-        lcd.print("Disconnected");
-        
-      }
-      else {
-        //Dislays and gets data from the sensor
-        lcd.setCursor(0, 0);
-        celsius_temp="Celsius:"+String(temp_celsius)+"C";
-        farenheit_temp="Farenheit:"+String(temp_farenheit)+"F";
-        lcd.print(celsius_temp);
-        lcd.setCursor(0, 1);
-        lcd.print(farenheit_temp);
-      }
-    }
-    else
-    {
-      lcd.clear();
-    }
-    button_value = 0;
+
+
+  //Reading pi data.
+  if (Serial.available() > 0) {
+    switch_fb = Serial.readStringUntil('\n');
+    delay(30);
   }
 
-  else {
-    //stop sending value to pi
+  if (switch_fb == "true") {
 
+    if (temp_celsius != -127) {
+
+      if (switch_value == HIGH) {
+        // Send a value to pi to start up data in firebase
+        // And start listening to arduino for temp
+
+        // Serial.println("Switch on");
+
+        // Serial.println("Connected");
+        Serial.println(temp_celsius);
+        if (button_value == HIGH) {
+          //Dislays and gets data from the sensor
+          lcd.setCursor(0, 0);
+          celsius_temp = "Celsius:" + String(temp_celsius) + "C";
+          farenheit_temp = "Farenheit:" + String(temp_farenheit) + "F";
+          lcd.print(celsius_temp);
+          lcd.setCursor(0, 1);
+          lcd.print(farenheit_temp);
+
+        } else {
+          lcd.clear();
+        }
+      } else {
+        lcd.clear();
+        Serial.println("Switch off");
+      }
+
+    } else {
+      //stop sending value to pi
+      lcd.setCursor(0, 0);
+      lcd.print("Disconnected");
+      delay(500);
+      Serial.println("Disconnected");
+    }
+  } else {
+    if (switch_value == LOW) {
+      switch_fb = "true";
+      Serial.println("Switch off");
+    }
   }
-
-
-
+  Serial.flush();
 }
