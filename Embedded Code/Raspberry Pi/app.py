8a43761f97e230e0e708ff32e3bba7e480023dfa
Adnane Ezouhri <adnane-ezouhri@uiowa.edu>
2022-09-28 01:39:17 -0500

    Adding code from pi

--- Embedded Code/Raspberry Pi/app.py
+++ Embedded Code/Raspberry Pi/app.py
@@ -19,63 +19,48 @@ firebase=pyrebase.initialize_app(fbConfig)
 db = firebase.database()
 
 
-
-
 ser = serial.Serial('/dev/ttyACM0',9600)
 s = ''
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 ser.reset_input_buffer()
 
 
-# def switch_listen(message):
-#     oneuh=db.child("Lab1/Is_On").get().val()
-#     print(oneuh)
 
 temp=True
 while True:
-    # Works fine
-    
-    # ser.flushinput()
         
-    test=db.child("Lab1/Server_Button").get().val()
+    server_button_value=db.child("Lab1/Server_Button").get().val()
 
-    if(test!=temp):
-        if(test== False):
+    if(server_button_value!=temp):
+        if(server_button_value== False):
             ser.write("false\n".encode('utf-8'))
         else:
             ser.write("true\n".encode('utf-8'))
-        temp=test
+
+        temp=server_button_value
         
     if (ser.in_waiting > 0):
     
-        line = ser.readline().decode('utf-8').rstrip()
-
-        if(line=="Switch off"):
+        arduino_message = ser.readline().decode('utf-8').rstrip()
+        print(arduino_message)
+        if(arduino_message=="Switch off"):
             db.child("Lab1").update({"Is_On":False})
             db.child("Lab1").update({"Server_Button":True})
             complete= db.child("Lab1/Temp_History").get().val()+",null"
             db.child("Lab1").update({"Temp_History":complete})
     
-        elif(line=="Disconnected"):
+        elif(arduino_message=="Disconnected"):
             db.child("Lab1").update({"Is_connected":False})
+            complete= db.child("Lab1/Temp_History").get().val()+",null"
+            db.child("Lab1").update({"Temp_History":complete})
+
+        elif(arduino_message=="Server off"):
             complete= db.child("Lab1/Temp_History").get().val()+",null"
             db.child("Lab1").update({"Temp_History":complete})
 
         else:
             db.child("Lab1").update({"Is_connected":True})
             db.child("Lab1").update({"Is_On":True})
-            db.child("Lab1").update({"Temperature":line})
-            complete= db.child("Lab1/Temp_History").get().val()+','+line
+            db.child("Lab1").update({"Temperature":arduino_message})
+            complete= db.child("Lab1/Temp_History").get().val()+','+arduino_message
             db.child("Lab1").update({"Temp_History":complete})
-
-
-
-        
-
-
-# db.child("Lab1/Is_connected").stream(switch_listen)
-    
-
-
-    
-    
